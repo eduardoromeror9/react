e72f1acccd26402bd9f50ad7e77df1d6d9f33c5d
Eduardo Romero <eduardoromeror9@outlook.com>
2022-06-14 19:24:20 -0400

    Proyecto para gastos

---
+++ control-gastos/src/components/Modal.jsx
@@ -0,0 +1,33 @@
+import CerrarModal from '../img/cerrar.svg'
+
+
+const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+
+  const ocultarModal = () => {
+    setAnimarModal(false)
+
+    setTimeout(() => {
+      setModal(false)
+    }, 500);
+  
+  }
+
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img 
+          src={ CerrarModal } 
+          alt="cerrar modal"
+          onClick={ ocultarModal }
+        />
+      </div>
+
+      <form className={`formulario ${ animarModal ? "animar" : 'cerrar' }`}>
+        <legend>Nuevo Gasto</legend>
+      </form>
+
+    </div>
+  )
+}
+
+export default Modal
