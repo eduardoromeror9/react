5dc9986831013795ec9fad7c32564fffa43b603c
Eduardo Romero <eduardoromeror9@outlook.com>
2022-06-18 10:55:01 -0400

    crear un filtro para ver los gastos

--- control-gastos/src/components/ControlPresupuesto.jsx
+++ control-gastos/src/components/ControlPresupuesto.jsx
@@ -1,16 +1,29 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
+  const [porcentaje, setPorcentaje] = useState(0)
+
   const [disponible, setDisponible] = useState(0)
   const [gastado, setGastado] = useState(0)
+
 
   useEffect(() => {
     const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
     const totalDisponible = presupuesto - totalGastado
 
+    // Calcular porcentaje
+    const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2)
+    
     setDisponible(totalDisponible)
     setGastado(totalGastado)
+
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje)
+    }, 1200)
+
   }, [gastos])
 
   const formatearCantidad = (cantidad) => {
@@ -24,7 +37,16 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>Grafica Aqui</p>
+
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#3B82F6',
+            textColor: '#3B82F6',
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
+        />
+        
       </div>
 
       <div className="contenido-presupuesto">
