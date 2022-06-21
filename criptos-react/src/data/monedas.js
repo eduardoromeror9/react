2db10204d5ed5afd038c4f38ac5353635ca16edf
Eduardo Romero <eduardoromeror9@outlook.com>
2022-06-21 19:05:47 -0400

    Validar el formulario

---
+++ criptos-react/src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+  { id: 'USD', nombre: 'Dolar Americano' },
+  { id: 'CLP', nombre: 'Peso Chileno' },
+  { id: 'MXN', nombre: 'Peso Mexicano' },
+  { id: 'EUR', nombre: 'Euro' },
+]
+
+export {
+  monedas
+}
