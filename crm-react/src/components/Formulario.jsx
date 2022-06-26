a72c89d02857669578fb12219022811aa66bd3e6
Eduardo Romero <eduardoromeror9@outlook.com>
2022-06-26 02:43:19 -0400

    Actualizar el registro a la API

--- crm-react/src/components/Formulario.jsx
+++ crm-react/src/components/Formulario.jsx
@@ -3,9 +3,10 @@ import { Formik, Form, Field } from 'formik'
 import { useNavigate } from 'react-router-dom'
 import * as Yup from 'yup';
 import Alerta from './Alerta';
+import Spinner from './Spinner';
 
 
-const Formulario = () => {
+const Formulario = ({ cliente, cargando }) => {
 
   const navigate = useNavigate()
 
@@ -47,17 +48,20 @@ const Formulario = () => {
   }
 
   return (
+
+    cargando ? <Spinner /> : (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
-      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Agregar Cliente</h1>
+      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">{cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}</h1>
 
       <Formik
         initialValues={{
-          nombre: "",
-          empresa: "",
-          email: "",
-          telefono: "",
-          notas: "",
+          nombre: cliente?.nombre ?? "",  // nullish coalescing operator
+          empresa: cliente?.empresa ?? "",
+          email: cliente?.email ?? "",
+          telefono: cliente?.telefono ?? "",
+          notas: cliente?.notas ?? "",
         }}
+        enableReinitialize={true}
         onSubmit={ async ( values, {resetForm} ) => {
           await handleSubmit( values )
 
@@ -162,7 +166,7 @@ const Formulario = () => {
 
           <input
             type="submit"
-            value="Agregar Cliente"
+            value={cliente?.nombre ? 'Editar Cliente' : 'Agregar Cliente'}
             className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg rounded-lg cursor-pointer"
           />
 
@@ -170,7 +174,13 @@ const Formulario = () => {
       )}}
       </Formik>
     </div>
+    )
   )
+}
+
+Formulario.defaultProps = {
+  cliente: {},
+  cargando: false
 }
 
 export default Formulario
