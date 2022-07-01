071bb93973159580455b9d23661a70fab24899e9
Eduardo Romero <eduardoromeror9@outlook.com>
2022-07-01 01:11:06 -0400

    Añadir informacion al resultado

--- cotizador/src/components/AppSeguro.jsx
+++ cotizador/src/components/AppSeguro.jsx
@@ -1,9 +1,11 @@
 import Formulario from "./Formulario"
-// import Spinner from "./Spinner"
-// import Resultado from "./Resultado"
-// import useCotizador from "../hooks/useCotizador"
+import Spinner from "./Spinner"
+import Resultado from "./Resultado"
+import useCotizador from "../hooks/useCotizador"
 
 const AppSeguro = () => {
+
+  const { resultado, cargando } = useCotizador()
 
   return (
     <>
@@ -14,7 +16,7 @@ const AppSeguro = () => {
         <main className="bg-white md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10">
             <Formulario />
 
-            {/* {cargando  ? <Spinner /> : <Resultado />} */}
+            {cargando  ? <Spinner /> : <Resultado/> }
         </main>
 
     </>
--- cotizador/src/components/Formulario.jsx
+++ cotizador/src/components/Formulario.jsx
@@ -1,14 +1,28 @@
 import { Fragment, useContext } from "react"
 import { MARCAS, YEARS, PLANES } from "../constans"
 import useCotizador from "../hooks/useCotizador"
+import Error from "./Error"
 
 
 const Formulario = () => {
 
+  const { datos, handleChangeDatos, error, setError, cotizarSeguro } = useCotizador()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(Object.values(datos).includes('')) {
+      setError('Todos los campos son obligatorios')
+      return
+    }
+    setError('')
+
+    cotizarSeguro()
+  }
 
   return (
     <>
-      <form>
+      {error && <Error />}
+      <form onSubmit={ handleSubmit }>
         <div className="my-5">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
             Marca
@@ -17,6 +31,8 @@ const Formulario = () => {
             name="marca" 
             className="w-full p-3 bg-white border 
             border-gray-200"
+            onChange={e => handleChangeDatos(e)}
+            value={datos.marca}
           >
             <option value="">-- Selecciona Marca --</option>
             {MARCAS.map(marca => (
@@ -35,9 +51,11 @@ const Formulario = () => {
             Año
           </label>
           <select 
-            name="marca" 
+            name="year" 
             className="w-full p-3 bg-white border 
             border-gray-200"
+            onChange={e => handleChangeDatos(e)}
+            value={datos.year}
           >
             <option value="">-- Selecciona Año --</option>
             {YEARS.map(year => (
@@ -65,6 +83,7 @@ const Formulario = () => {
                   type="radio"
                   name="plan"
                   value={plan.id}
+                  onChange={e => handleChangeDatos(e)}
                 />
               </Fragment>
             ))}
