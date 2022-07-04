20b17b58c67ef4c7601084779a702c97c22a1b82
Eduardo Romero <eduardoromeror9@outlook.com>
2022-07-04 14:34:39 -0400

    Proyecto finalizado

--- letrasCanciones/src/App.jsx
+++ letrasCanciones/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import AppLetras from './components/AppLetras'
+import { LetrasProvider } from './context/LetrasProvider'
 
 
 function App() {
 
   return (
-    <>
-    </>
+    <LetrasProvider>
+      <AppLetras/>
+    </LetrasProvider>
   )
 }
 
